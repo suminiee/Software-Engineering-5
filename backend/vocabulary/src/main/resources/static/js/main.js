fb75106ef0b04948846920aaafdffb97174fe5df
keemzleun <92363679+keemzleun@users.noreply.github.com>
2023-05-23 19:42:44 +0900

    마이페이지(수정중)

--- backend/vocabulary/src/main/resources/static/js/main.js
+++ backend/vocabulary/src/main/resources/static/js/main.js
@@ -1,10 +1,34 @@
-function service1() {
+/*
+function voca() {
   let signIn = "${signIn}";
   if (signIn == "") {
     alert("로그인 후 이용할 수 있습니다.");
-    location.href="${cpath}/login.do"
+    location.href="${cpath}/login.do"  // 경로 수정해야 함
   }
   else {
-    location.href="${cpath}/service.do"
+    location.href="${cpath}/service.do"  // 경로 수정해야 함
   }
 }
+
+function test() {
+  let signIn = "${signIn}";
+  if (signIn == "") {
+    alert("로그인 후 이용할 수 있습니다.");
+    location.href="${cpath}/login.do"  // 경로 수정해야 함
+  }
+  else {
+    location.href="${cpath}/service.do"  // 경로 수정해야 함
+  }
+}
+
+function group() {
+  let signIn = "${signIn}";
+  if (signIn == "") {
+    alert("로그인 후 이용할 수 있습니다.");
+    location.href="${cpath}/login.do"  // 경로 수정해야 함
+  }
+  else {
+    location.href="${cpath}/service.do"  // 경로 수정해야 함
+  }
+}
+*/
